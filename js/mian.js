f4bcb6cc2003f483f206780a37cdf21cac5a5c93
NicholasConti <conti94@live.it>
2023-03-01 10:16:05 +0100

    add do-while

--- js/mian.js
+++ js/mian.js
@@ -3,6 +3,7 @@
 const list = ['Pane', 'Carote', 'Patate', 'Burro'];
 const ul = document.getElementById('shoppig-list');
 
+//WHILE
 let i = 0;
 while (i < list.length){
     const li = document.createElement('li');
@@ -10,3 +11,12 @@ while (i < list.length){
     li.append(list[i]);
     i++;
 }
+
+//DO WHILE
+i= 0;
+do {
+    const li = document.createElement('li');
+    ul.append(li);
+    li.append(list[i]);
+    i++;
+} while (i < list.length);
