fec742b52757ee8adbeb6e9e35aaf0d34c99bbd3
NicholasConti <conti94@live.it>
2023-03-01 10:07:08 +0100

    fix

--- js/mian.js
+++ js/mian.js
@@ -1,10 +1,10 @@
 "use strict";
 
 const list = ['Pane', 'Carote', 'Patate', 'Burro'];
+const ul = document.getElementById('shoppig-list');
 
 let i = 0;
 while (i < list.length){
-    const ul = document.getElementById('shoppig-list');
     const li = document.createElement('li');
     ul.append(li);
     li.append(list[i]);
